4ad677c544e491ec959364231fb89fe47d9ff11e
Łukasz Rutkowski <lukus178@gmail.com>
2019-08-17 11:40:12 +0200

    Migrate to AndroidX (#44)

--- app/src/main/java/me/saket/dank/ui/user/UserSessionRepository.java
+++ app/src/main/java/me/saket/dank/ui/user/UserSessionRepository.java
@@ -1,11 +1,7 @@
 package me.saket.dank.ui.user;
 
-import static io.reactivex.android.schedulers.AndroidSchedulers.mainThread;
-import static io.reactivex.schedulers.Schedulers.io;
-
-import android.content.Context;
-import android.support.annotation.CheckResult;
-import android.support.annotation.Nullable;
+import androidx.annotation.CheckResult;
+import androidx.annotation.Nullable;
 
 import com.f2prateek.rx.preferences2.Preference;
 import com.f2prateek.rx.preferences2.RxSharedPreferences;
