a6d0d5111c9feab8f0af66b6ab941a2622f43676
mfgm2 <repom2@airmail.cc>
2020-09-12 00:54:41 +0300

    Remove drawable caching

--- app/src/main/java/me/saket/dank/widgets/span/CenterAlignedImageSpan.kt
+++ app/src/main/java/me/saket/dank/widgets/span/CenterAlignedImageSpan.kt
@@ -7,30 +7,11 @@ import android.graphics.drawable.Drawable
 import android.text.style.ImageSpan
 import java.lang.ref.WeakReference
 
-class CenterAlignedImageSpan(d: Drawable, private val lineSpacingExtra: Int): ImageSpan(d, ALIGN_BASELINE) {
-  private var drawableReference: WeakReference<Drawable>? = null
-
-  // Reimplemented because getCachedDrawable above uses field from this class
-  override fun getSize(paint: Paint, text: CharSequence?,
-                       start: Int, end: Int,
-                       fm: FontMetricsInt?): Int {
-    val drawable = getCachedDrawable()
-    val rect = drawable.bounds
-
-    if (fm != null) {
-      fm.ascent = -rect.bottom
-      fm.descent = 0
-      fm.top = fm.ascent
-      fm.bottom = 0
-    }
-
-    return rect.right
-  }
+class CenterAlignedImageSpan(drawable: Drawable, private val lineSpacingExtra: Int): ImageSpan(drawable, ALIGN_BASELINE) {
 
   override fun draw(canvas: Canvas, text: CharSequence?,
                     start: Int, end: Int, x: Float,
                     top: Int, y: Int, bottom: Int, paint: Paint) {
-    val drawable = getCachedDrawable()
     val drawableHeight = drawable.bounds.height()
     val translationY = (bottom - top - drawableHeight - lineSpacingExtra) / 2f
 
@@ -38,17 +19,5 @@ class CenterAlignedImageSpan(d: Drawable, private val lineSpacingExtra: Int): Im
     canvas.translate(x, translationY)
     drawable.draw(canvas)
     canvas.restore()
-  }
-
-  // Reimplemented because this is a private member
-  private fun getCachedDrawable(): Drawable {
-    var cachedDrawable = drawableReference?.get()
-
-    if (cachedDrawable == null) {
-      cachedDrawable = drawable
-      drawableReference = WeakReference(cachedDrawable)
-    }
-
-    return cachedDrawable!!
   }
 }
